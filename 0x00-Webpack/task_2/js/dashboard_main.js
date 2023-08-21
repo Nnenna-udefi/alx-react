53dcd2410dcc82df960522dc56c1fbbdc7917a17
Nnenna Udefi <miriamudefi@gmail.com>
2023-08-21 22:32:13 +0100

    loquash debounce import

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import debounce from 'lodash/debounce';
+import _ from 'lodash';
 import '../css/main.css';
 
 $(document).ready(function() {
@@ -22,7 +22,8 @@ $(document).ready(function() {
     $displayCount.text(`${count} clicks on the button`);
   }
   // Use jQuery's .on() method to attach the event listener
-  $btn.on('click', debounce(updateCounter, 300));
+  $btn.on('click', _.debounce(updateCounter, 300,
+    { leading: true, trailing: false }));
 })
 
 
