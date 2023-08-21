3eee408d375a67a8abf27912a607db366de983f5
Nnenna Udefi <miriamudefi@gmail.com>
2023-08-21 23:59:17 +0100

    editing jquery

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,20 +2,22 @@ const $ = require( "jquery" );
 // Load the full build
 const _ = require("lodash");
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
 let count = 0;
+// select the button element
+let $btn = $('button');
+// select the count display paragraph
+let $displayCount = $('#count');
 
-function updateCounter() {
+// function that tracks the number of times the button element has been clicked.
+const updateCounter = () => {
   count++;
-  $( "#count" ).text(`${count} clicks on the button` );
+  $displayCount.text(`${count} clicks on the button`);
 }
-
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce( updateCounter, 500, { leading: true, trailing: false })
-);
-
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append($button);
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+// Use jQuery's .on() method to attach the event listener
+$btn.on('click', _.debounce(updateCounter, 300));
