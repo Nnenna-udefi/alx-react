d7a2ab4936866fb71eb4812b878d639e58779267
Nnenna Udefi <miriamudefi@gmail.com>
2023-08-23 22:12:43 +0100

    removed unused files

--- 0x00-react_introduction/task_0/dashboard/src/App.js
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -9,9 +9,9 @@ function App() {
         <h1>School dashboard</h1>
       </header>
 
-      <body className='App-body'>
+      <div className='App-body'>
         <p>Login to access the full dashboard</p>
-      </body>
+      </div>
 
       <footer className='App-footer'>
         <p>Copyright 2020 - holberton School</p>
