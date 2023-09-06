62dad4c1bc33415a770a11f20b2debc33164ce48
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-06 20:36:05 +0100

    StyleSheetTest

--- 0x04-React_inline_styling/task_2/dashboard/src/App/App.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/App/App.test.js
@@ -10,8 +10,8 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-
 import { StyleSheetTestUtils } from "aphrodite";
+
 
 beforeEach(() => {
   StyleSheetTestUtils.suppressStyleInjection();
