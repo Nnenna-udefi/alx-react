23af157d9bca6ec90d260c9df3025bdf7e2d9554
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-13 18:53:40 +0100

    defaul

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,9 +1,8 @@
 import { Immutable } from 'immutable';
 
-const getImmutableObject = (object) => {
+export default function getImmutableObject(object) {
   const immutableMap = Immutable.Map(object);
   // Use fromJS to convert the input object into an immutable Map
   return immutableMap;
 };
 
-export default getImmutableObject;
