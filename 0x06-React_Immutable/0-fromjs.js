0c67ef63068c35519a0a19cbf0cd64a6a59a5485
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-13 18:48:53 +0100

    function

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,5 @@
-import { Immutable } from 'immutable';
+import { fromJS } from 'immutable';
 
-const getImmutableObject = (object) => {
-  const immutableMap = Immutable.fromJS(object);
-  // Use fromJS to convert the input object into an immutable Map
-  return immutableMap;
-};
-
-export default getImmutableObject;
+export default function getImmutableObject(object) {
+  return fromJS(object);
+}
