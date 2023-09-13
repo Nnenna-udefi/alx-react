e24cadd6476a1c3412df716182a310b7625e1f3d
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-13 20:16:50 +0100

    added comments

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,6 @@
 import { fromJS } from 'immutable';
 
+// converts the object into an immutable Map using fromJS of the Immutable.js library
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,6 @@
 import { Map } from 'immutable';
 
+// converts it into an immutable Map using Map of the Immutable.js library
 export default function getImmutableObject(object) {
   return Map(object);
 };
