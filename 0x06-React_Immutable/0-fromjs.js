c6953d09a6a303953ab95de87ab42906fbf0143b
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-13 18:33:33 +0100

    readme

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,9 @@
+import Immutable from 'immutable';
+
+const getImmutableObject = (object) => {
+    const immutableMap = Immutable.fromJS(object);
+    // Use fromJS to convert the input object into an immutable Map
+    return immutableMap;
+};
+
+module.exports = getImmutableObject;
---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,9 @@
+import Immutable from 'immutable';
+
+const getImmutableObject = (object) => {
+    const immutableMap = Immutable.Map(object);
+    // Use fromJS to convert the input object into an immutable Map
+    return immutableMap;
+};
+
+module.exports = getImmutableObject;
