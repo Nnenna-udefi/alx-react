cff4e3910629c891316d8e8671aab0bddd9c501e
Nnenna Udefi <miriamudefi@gmail.com>
2023-09-25 22:50:07 +0100

    Adding immutable.js to the reducers

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,40 +1,24 @@
 import { LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
+import { Map } from 'immutable';
 
-const initialState = {
+// Initial state as an Immutable Map
+const initialState = Map({
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
-    user: {},
-};
+    user: Map({}),
+});
 
 const uiReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'DISPLAY_NOTIFICATION_DRAWER':
-            return {
-                // Returns new state object with (...state)
-                // that has all the existing state data
-                ...state,
-                isNotificationDrawerVisible: true,
-            };
+            return state.set('isNotificationDrawerVisible', true);
         case 'HIDE_NOTIFICATION_DRAWER':
-            return {
-                ...state,
-                isNotificationDrawerVisible: false,
-            };
+            return state.set('isNotificationDrawerVisible', false); 
         case 'LOGIN_SUCCESS':
-            return {
-                ...state,
-                isUserLoggedIn: true,
-            };
+            return state.set('isUserLoggedIn', true);
         case 'LOGIN_FAILURE':
-            return {
-                ...state,
-                isUserLoggedIn: false,
-            };
         case 'LOGOUT':
-            return {
-                ...state,
-                isUserLoggedIn: false
-            };
+            return state.set('isUserLoggedIn', false);
         default:
             // If this reducer doesn't recognize the action type, or doesn't
             // care about this specific action, return the existing state unchanged
